b1a3e6e2bcf03468e3b54f8865ac42e988984967
rnzid <ranjitdangol13@gmail.com>
2023-05-22 14:56:16 +0545

    password encription

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,4 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -7,11 +9,18 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("User", UserSchema);
