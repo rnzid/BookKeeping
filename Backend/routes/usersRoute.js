8592dd533f5d4a4be91170265a505a4698157b43
rnzid <ranjitdangol13@gmail.com>
2023-05-22 15:35:13 +0545

    login route working

--- Backend/routes/usersRoute.js
+++ Backend/routes/usersRoute.js
@@ -13,27 +13,44 @@ usersRoute.post('/register',asyncHandler(async(req,res)=>{
         throw new Error('User exists')
     }
 const userCreated = await User.create({name, email, password});
+res.send(userCreated);
 }));
 
-// async (req, res)=>{
-//     try {
+/* async (req, res)=>{
+    try {
         
-//         //console.log(req.body);
-//         const {name, email, password} = req.body;
-//         const user = await User.create({name, email, password});
-//         console.log(user);
-//         res.send(user);
+        //console.log(req.body);
+        const {name, email, password} = req.body;
+        const user = await User.create({name, email, password});
+        console.log(user);
+        res.send(user);
         
-//     }catch(error){
-//         res.send(error);
-//     }
+    }catch(error){
+        res.send(error);
+    }
 
-// }
+} */
 
 //login
-usersRoute.post('/login',(req, res)=>{
-    res.send('login Route')
-});
+usersRoute.post('/login',asyncHandler(async(req,res)=>{
+    const {email,password}=req.body;
+
+    const user = await User.findOne({email});
+    if(!user){
+        res.status(401);
+        throw new Error('invalid Credentials')
+    }else{
+        //set statuscode
+        res.status(200);
+        res.json({
+            _id: user.id,
+            name:user.name,
+            password: user.password,
+            email: user.email,
+
+        })
+    }
+}));
 
 //Update
 usersRoute.put('/update',(req, res)=>{
