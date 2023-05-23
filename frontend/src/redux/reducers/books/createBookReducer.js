594b16d63b5463c3b1aea2d0288e59a6a2173341
rnzid <ranjitdangol13@gmail.com>
2023-05-23 20:54:19 +0545

    redux reducer state action trigger  using swicth

---
+++ frontend/src/redux/reducers/books/createBookReducer.js
@@ -0,0 +1,22 @@
+const { CREATE_BOOK_REQUEST, CREATE_BOOK_FAIL } = require("../../action/actionTypes");
+
+const createBookReducer = (state={},action)=>{
+    switch(action.type){
+        case CREATE_BOOK_REQUEST:
+            return {
+                loading: true,
+            };
+        case CREATE_BOOK_SUCCESS:
+            return{
+                book: action.payload
+            };
+        case CREATE_BOOK_FAIL:
+            return{
+                loading: false,
+                error: action.payload,
+            };
+        default:
+            return state;
+
+    }
+}
