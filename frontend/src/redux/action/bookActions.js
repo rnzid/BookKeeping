56150652290ffabd9d49d70abc35f48d23194553
rnzid <ranjitdangol13@gmail.com>
2023-05-23 20:46:31 +0545

    Book create redux action trigger

---
+++ frontend/src/redux/action/bookActions.js
@@ -0,0 +1,32 @@
+const {
+  CREATE_BOOK_REQUEST,
+  CREATE_BOOK_SUCCESS,
+  CREATE_BOOK_FAIL,
+} = require("./actionTypes");
+
+const createBookAction = (bookData) => {
+  return async (dispatch) => {
+    try {
+      dispatch({
+        type: CREATE_BOOK_REQUEST,
+      });
+      const config = {
+        "content-Type": "application/json",
+      };
+
+      const { data } = await axios.post("/api/books", bookData, config);
+
+      dispatch({
+        type: CREATE_BOOK_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: CREATE_BOOK_FAIL,
+        payload: error.response && error.response.data.message,
+      });
+    }
+  };
+};
+
+export {createBookAction};
